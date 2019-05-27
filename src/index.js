a640632f8ab2200eef8f081478d5e217da4996eb
ktucalvin <ktucalvin@gmail.com>
2019-05-27 10:40:36 -0600

    Add fullscreen page reading

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ window.onpopstate = event => {
   const state = event.state
   const $galleries = document.getElementById('galleries')
   const $reader = document.getElementById('reader')
+  document.exitFullscreen().catch()
   if (state && state.isReading) {
     $reader.style.display = 'block'
     $galleries.style.display = 'none'
@@ -18,3 +19,12 @@ window.onpopstate = event => {
     $galleries.style.display = 'block'
   }
 }
+
+document.addEventListener('keyup', e => {
+  const $page = $('#reader img')[0]
+  if (e.key === 'f' && $page) {
+    $page.requestFullscreen()
+  } else if (e.key === 'Esc') {
+    document.exitFullscreen()
+  }
+})
