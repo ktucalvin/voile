77fed9ccad760b06d30a67b84de38e464ebacbf5
ktucalvin <ktucalvin@gmail.com>
2019-06-30 21:51:15 -0600

    Implement etag and set maxage headers

--- index.js
+++ index.js
@@ -5,26 +5,32 @@ const Koa = require('koa')
 const serve = require('koa-static')
 const mount = require('koa-mount')
 const send = require('koa-send')
+const conditional = require('koa-conditional-get')
+const etag = require('koa-etag')
 const routes = require('./lib/routes')
 const app = new Koa()
 const certopts = {
   key: process.env.SSL_KEY,
   cert: process.env.SSL_CERT
 }
+const staticOpts = { maxage: 86400000 } // 1 day
+
+app.use(conditional())
+app.use(etag())
 
 app.use(routes)
 
-app.use(serve('./dist'))
+app.use(serve('./dist', staticOpts))
 
-app.use(mount('/g', serve(process.env.ARCHIVE_DIR, { maxage: 86400 })))
+app.use(mount('/g', serve(process.env.ARCHIVE_DIR, staticOpts)))
 
 app.use(async ctx => {
   if (ctx.path.endsWith('bundle.js')) {
-    await send(ctx, 'dist/bundle.js')
+    await send(ctx, 'dist/bundle.js', staticOpts)
   } else if (ctx.path.endsWith('main.css')) {
-    await send(ctx, 'dist/main.css')
+    await send(ctx, 'dist/main.css', staticOpts)
   } else {
-    await send(ctx, 'dist/index.html')
+    await send(ctx, 'dist/index.html', staticOpts)
   }
 })
 
